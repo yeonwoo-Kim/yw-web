a294b981f3a8ad4c72b6763c281a80c862f353f2
김연우 <yeonwookim@gim-yeon-us-MacBook-Air.local>
2023-01-28 22:44:22 +0900

    Initial commit

---
+++ src/main/java/dev/ywweb/YwWebApplication.java
@@ -0,0 +1,13 @@
+package dev.ywweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YwWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YwWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/ywweb/YwWebApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ywweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YwWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
